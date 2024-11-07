e9a18a4740faeabd155b80b005f849a2660b7a62
Ulfred-prog <alfred.sixtensson@skola.taby.se>
2024-11-07 10:02:31 +0100

    Update äventyrsspel.py2

--- äventyrsspel.py
+++ äventyrsspel.py
@@ -1,2 +1,12 @@
-print("Hello")
-print("night")
+namn=input("Vad heter du?-->")
+
+class Player:
+    def HP(x):
+        x=100
+    def Lvl(y):
+        y=1
+    
+    def _init_(self,namn, HP, Lvl):
+        self.name = namn
+        self.hp=HP
+print(Player)
