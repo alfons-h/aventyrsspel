28b503a6f1454e4ee8d4749da8250041b65d1dc5
Ulfred-prog <alfred.sixtensson@skola.taby.se>
2024-11-07 10:46:31 +0100

    Update3 äventyrsspel.py

--- äventyrsspel.py
+++ äventyrsspel.py
@@ -1,12 +1,49 @@
-namn=input("Vad heter du?-->")
+name = input("Vad heter du?-->")
 
 class Player:
-    def HP(x):
-        x=100
-    def Lvl(y):
-        y=1
+    def __init__(self, name, lvl, hp):
+        self.name = name
+        self.hp = hp
+        self.lvl = lvl
     
-    def _init_(self,namn, HP, Lvl):
-        self.name = namn
-        self.hp=HP
-print(Player)
+        print("Player name: " + self.name)
+        print("Player hp: " + str(self.hp))
+        print("Level: " + str(self.lvl))
+
+player = Player(name, 1, 100)
+
+class inventory:
+    def __init__(self):
+        self.items = []
+        
+    def add_item(self,item_name, quantity=1):
+        if item_name in self.items:
+            self.items[item_name] +=quantity
+        else:
+            self.items[item_name] = quantity
+        print(f"added {quantity} {item_name} to inventory")
+
+        def remove_item(self,item_name,quantity=1):
+            if item_name in self.items:
+                if self.items[item_name] > quantity:
+                    self.items[item_name] -=quantity
+                    print(f"{quantity} {item_name} was removed")
+
+        
+
+
+
+        
+
+
+
+
+
+
+
+
+class Creature:
+    def __init__(monster,name,hp,lvl):
+        monster.name = name
+        monster.hp = hp
+        monster.lvl = lvl
