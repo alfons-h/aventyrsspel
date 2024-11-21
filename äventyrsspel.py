e7738277661e0bcc891857b038cd00268023622d
alfons-h <alfons.hermansson@skola.taby.se>
2024-11-21 09:35:21 +0100

    Add files via upload

--- äventyrsspel.py
+++ äventyrsspel.py
@@ -3,10 +3,11 @@ name = input("Vad heter du?--> ")
 
 
 class Player:
-    def __init__(self, name, lvl, hp):
+    def __init__(self, name, lvl, hp, armor):
         self.name = name.capitalize()
         self.hp = hp
         self.lvl = lvl
+        self.armor = armor
         print("Välkommen " + self.name + " till grottkravlare.")
         print()
         print("     Spelar Stats     ")
@@ -14,10 +15,12 @@ class Player:
         print("Player Name: " + (self.name))
         print("Player HP: " + str(self.hp))
         print("Level: " + str(self.lvl))
+        print("Armor:" + str(self.armor))
         print("══════════════════════")
 
 
-player = Player(name, 1, 100)
+player = Player(name, 1, 100, 20)
+
 
 class Inventory:
     def __init__(self):
@@ -46,23 +49,24 @@ class Inventory:
             
 
 class Monster:
-    def __init__(self, name, hp, lvl):
+    def __init__(self, name, hp, lvl, bild):
         self.name = name
         self.hp = hp
         self.lvl = lvl
+        self.bild = bild
 
     def __str__(self):
-        return f"{self.name}: HP {self.hp}, Level {self.lvl}"
+        return f"{self.name}: HP {self.hp}, Level {self.lvl}, {self.bild}"
 
 monsters = [
-    Monster("Råtta", 3, 1),
-    Monster("Slime", 10, 3),
-    Monster("Goblin", 25, 7),
-    Monster("Zombie", 50, 10),
-    Monster("Spöke", 75, 12),
-    Monster("Vampyr", 100, 15),
-    Monster("Varulv", 150, 20),
-    Monster("Drake", 500, 100)
+    Monster("Råtta", 3, 1, "LÄGG IN BILD HÄR"),
+    Monster("Slime", 10, 3, "LÄGG IN BILD HÄR"),
+    Monster("Goblin", 25, 7, "LÄGG IN BILD HÄR"),
+    Monster("Zombie", 50, 10, "LÄGG IN BILD HÄR"),
+    Monster("Spöke", 75, 12, "LÄGG IN BILD HÄR"),
+    Monster("Vampyr", 100, 15, "LÄGG IN BILD HÄR"),
+    Monster("Varulv", 150, 20, "LÄGG IN BILD HÄR"),
+    Monster("Drake", 500, 100, "LÄGG IN BILD HÄR")
 ]
 
 current_monster = random.choice(monsters) # Får fixa den här senare när vi har fixat rum och dörrar o sånt
@@ -71,7 +75,7 @@ print(f"Du stöter på {current_monster.name}.")
 print(f"{current_monster.name} har {current_monster.hp} HP")
 
 def monster_attack(current_monster, player):
-    damage = random.randint(3, 10) * current_monster.lvl // 2
+    damage = random.randint(3, 10) * current_monster.lvl
     print(f"{current_monster.name} attackerar dig och gör {damage} skada!")
     player.hp = max(0, player.hp - damage)
     if player.hp > 0:
